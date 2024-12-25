7f506f11f87659f9cb455e9d366cf690584c8168
skypse <gabrieldoamaral.alves12@gmail.com>
2024-12-25 11:38:55 -0300

    Adicionar arquivos de projeto.

---
+++ PraticasAluraCsharp/Exercicios.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace PraticasAluraCsharp
+{
+    public class Exercicios
+    {
+        public void Exercicio1()
+        {
+            Console.WriteLine("Executando Exercício 1...");
+            Console.WriteLine("");
+            String curso = "Criando sua primeira aplicação com CSharp";
+            String nome = "Gabriel";
+            String sobrenome = "do Amaral";
+            String instrutoresDoCurso = "Guilherme Lima & Daniel Portugal";
+            Console.WriteLine(curso);
+            Console.WriteLine($"{nome} {sobrenome}");
+            Console.WriteLine(instrutoresDoCurso);
+            Console.WriteLine("");
+        }
+
+        public void Exercicio2()
+        {
+            Console.WriteLine("Executando Exercício 2...");
+            // Código do exercício 2
+        }
+
+        public void Exercicio3()
+        {
+            Console.WriteLine("Executando Exercício 3...");
+            // Código do exercício 3
+        }
+    }
+}
---
+++ PraticasAluraCsharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraticasAluraCsharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // instanciando a classe dos exercicios
+            Exercicios exercicios = new Exercicios();
+
+            // chamando os exericios
+            exercicios.Exercicio1();
+
+            Console.WriteLine("Todos os exercícios foram executados.");
+        }
+    }
+}
